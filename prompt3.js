092cdd9ce5d487ddc67a94f22ef315a102a951f3
Louie Knolle <knollelw@gmail.com>
2022-10-08 10:20:27 -0400

    include prompt3 includes 1 count solution

--- prompt3.js
+++ prompt3.js
@@ -2,6 +2,7 @@
 //compare current and upcoming characters 
 //if both character !==, count ++ and concatenate
 
+//doesn't print counts of 1 for single letters
 const compressString = (inputString) => {
 	if(typeof inputString !== 'string' || inputString.length === 0 ) {
   	return 'Please enter valid string';
@@ -22,3 +23,22 @@ const compressString = (inputString) => {
   }
   return outputString;
 }
+
+
+//returns count of 1 for single characters 
+// const compressString = (inputString) => {
+// 	if(typeof inputString !== 'string' || inputString.length === 0 ) {
+//   	return 'Please enter valid string';
+//   } 
+  
+//   let outputString = '';
+//   let count = 0;
+  
+//   for(let i = 0; i <= inputString.length; i++) {
+//   	count ++;
+//     if(inputString[i] !== inputString[i+1]) {
+//     	outputString += inputString[i] + count;
+//       count = 0;
+//   }
+//   return outputString;
+// }
