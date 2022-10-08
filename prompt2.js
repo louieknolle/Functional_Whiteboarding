ddd80eb9b7b844089f93afc7c24894e42852b281
Louie Knolle <knollelw@gmail.com>
2022-10-08 09:06:00 -0400

    prompt2 solution

--- prompt2.js
+++ prompt2.js
@@ -1,3 +1,19 @@
 const removeArrayDuplicates = (inputArray) => {
+    if(Array.isArray(inputArray) === false) {
+        return 'This is array-neous!';
+    } else {  
+        var singleValuesArray = [];
+        inputArray.forEach(element => {
+            if (!singleValuesArray.includes(element)) {
+            singleValuesArray.push(element);
+            }
+        });
+        return singleValuesArray;
+    }
     
+}
+
+// with filter 
+const removeArrayDuplicates2 = (inputArray2) => {
+    return inputArray2.filter((element, index) => inputArray2.indexOf(element) === index);
 }
